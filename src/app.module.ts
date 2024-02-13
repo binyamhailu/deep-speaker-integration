51d4eaebf9f75deabea4807a28b3bd6968a3eafe
Demes Abera- Partner /Gebeya <demes.abera@partner.safaricom.et>
2024-02-13 13:43:40 +0300

    try

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         port: 5432,
         database: "mpesa_vybe",
         username: process.env.POSTGRES_VYBE_USER,
-        password: Buffer.from(process.env.POSTGRES_VYBE_PASSWORD, 'base64').toString('utf-8'),
+        password: "rEZEWaydRAnk",
         entities: [User],
         synchronize: true,
       }),
