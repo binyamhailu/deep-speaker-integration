66a47b9d067ac89c789d31f9cf3c25eb8b8e9930
Bushra Mustofa - Partner/Gebeya <bushra.mustofa@partner.safaricom.et>
2024-02-13 14:13:18 +0300

    add env vars

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('Database Configuration:');
+  console.log('Host:', configService.get<string>('POSTGRES_HOST'));
+  console.log('Port:', configService.get<number>('POSTGRES_PORT'));
+  console.log('Database:', configService.get<string>('POSTGRES_DB'));
+  console.log('Username:', configService.get<string>('POSTGRES_VYBE_USER'));
+  console.log('Password:', configService.get<string>('POSTGRES_VYBE_PASSWORD'));
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
+
+    
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
