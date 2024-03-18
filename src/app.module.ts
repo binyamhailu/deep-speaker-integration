8720742635f5f5b7967d10c5d286981c4d875dd4
Binyam.Gebrekidan <binyam.gebrekidan@partner.safaricom.et>
2024-03-18 11:31:27 +0300

    added mpesa config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { User } from './user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-require('dotenv').config();
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -17,14 +15,14 @@ require('dotenv').config();
       useFactory: (configService: ConfigService) => ({
 
         type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: configService.get<number>('POSTGRES_PORT'),
-        database: configService.get<string>('POSTGRES_DB'),
-        // host: "10.3.141.204",
-        // port: 5432,
-        // database: "mpesa_vybe",
-        username:configService.get<string>('POSTGRES_USER'),
-        password:configService.get<string>('POSTGRES_PASSWORD'),
+        // host: configService.get<string>('POSTGRES_HOST'),
+        // port: configService.get<number>('POSTGRES_PORT'),
+        // database: configService.get<string>('POSTGRES_DB'),
+        host: "10.3.141.204",
+        port: 5432,
+        database: "mpesa_vybe",
+        username: configService.get<string>('POSTGRES_VYBE_USER'),
+        password: process.env.POSTGRES_VYBE_PASSWORD.trim(),
         entities: [User],
         synchronize: true,
       }),
